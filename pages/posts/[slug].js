f01d0b74b0e08b3dd7113c3febb8a6d57ad6e9b6
Carlos A. Chávez Rosario <66329126+cchavezmx@users.noreply.github.com>
2021-07-07 21:12:22 -0500

    revision de params

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -4,33 +4,38 @@ import { getAllPosts, getPostBySlug } from '../api/apiBlog'
 const Posts = ({ post }) => {
   return (
   <>
+    { console.log(post)}
     <h1>Ya te medicaste</h1>
   </>
   )
 }
-
 export async function getStaticProps ({ params }) {
   const post = getPostBySlug(params.slug, [
     'title',
     'date',
-    'slug'
-
+    'slug',
+    'author',
+    'content',
+    'ogImage',
+    'coverImage'
   ])
   const content = await markdownToHtml(post.content || '')
 
   return {
-    post: {
-      ...post,
-      content
+    props: {
+      post: {
+        ...post,
+        content
+      }
     }
   }
 }
 
-export async function getStaticPatchs () {
+export async function getStaticPaths () {
   const posts = getAllPosts(['slug'])
 
   return {
-    paths: posts.map(post => {
+    paths: posts.map((post) => {
       return {
         params: {
           slug: post.slug
